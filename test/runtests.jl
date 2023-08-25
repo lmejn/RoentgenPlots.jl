ddbf521d2fcd5ace55ff3ed43c8e13c304d6c8ca
lmejn <33491793+lmejn@users.noreply.github.com>
2023-08-25 11:40:33 +1000

    Added MultiLeafCollimator tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,3 +6,14 @@ using RoentgenPlots
     jaws = Jaws(sort(rand(2)), sort(rand(2)))
     @test typeof(plot_bld(jaws)) <: RoentgenPlots.AbstractPlot
 end
+
+@testset "MultiLeafCollimator" begin
+    mlcx = [-20. 10. -8.  -3.
+            -10. 12.  12. 12.]
+    mlcy = -10:5.:10
+    mlc = MultiLeafCollimator(mlcx, mlcy)
+
+    @test typeof(plot_bld(mlc)) <: RoentgenPlots.AbstractPlot
+    @test typeof(plot_bld(mlc; invert=true)) <: RoentgenPlots.AbstractPlot
+    @test typeof(plot_bld(mlc; fill=false)) <: RoentgenPlots.AbstractPlot
+end
