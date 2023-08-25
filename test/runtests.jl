ee6aef22cdb78c7a0baed8075fbba82bed0f567d
lmejn <33491793+lmejn@users.noreply.github.com>
2023-08-25 11:58:48 +1000

    Minor cosmetic change

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,8 +6,8 @@ _check_type(p) = typeof(p) <: RoentgenPlots.AbstractPlot
 
 @testset "Jaws" begin
     jaws = Jaws(sort(rand(2)), sort(rand(2)))
-    @test _check_type(plot_bld(jaws))
-    @test _check_type(axes_lims!(jaws))
+    @test plot_bld(jaws) |> _check_type
+    @test axes_lims!(jaws) |> _check_type
 end
 
 @testset "MultiLeafCollimator" begin
@@ -16,19 +16,19 @@ end
     mlcy = -10:5.:10
     mlc = MultiLeafCollimator(mlcx, mlcy)
 
-    @test _check_type(plot_bld(mlc))
-    @test _check_type(plot_bld(mlc; invert=true))
-    @test _check_type(plot_bld(mlc; fill=false))
+    @test plot_bld(mlc) |> _check_type
+    @test plot_bld(mlc; invert=true) |> _check_type
+    @test plot_bld(mlc; fill=false) |> _check_type
 end
 
 @testset "Bixels" begin
     bixel = Bixel(rand(2), rand(2))
-    @test _check_type(plot_bld(bixel))
-    @test _check_type(plot_bld(bixel, rand()))
+    @test plot_bld(bixel) |> _check_type
+    @test plot_bld(bixel, rand()) |> _check_type
 
     bixels = [Bixel(i*rand(2), i*rand(2)) for i=1:2]
-    @test _check_type(plot_bld(bixels))
-    @test _check_type(plot_bld(bixels, rand(2)))
+    @test plot_bld(bixels) |> _check_type
+    @test plot_bld(bixels, rand(2)) |> _check_type
 
     bixels = BixelGrid(-10:5.:10,-2.:1.:2)
     @test plot_bld(bixels) |> _check_type
