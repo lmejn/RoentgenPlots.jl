8216ace2b51892f85f18d5e9d7b6d1f238063bef
lmejn <33491793+lmejn@users.noreply.github.com>
2023-08-25 11:43:33 +1000

    Moved bixel plotting to own file

--- src/RoentgenPlots.jl
+++ src/RoentgenPlots.jl
@@ -39,58 +39,6 @@ plot_bld!(args...; kwargs...) = plot_bld!(plot!(), args...; kwargs...)
 
 include("Jaws.jl")
 include("MultiLeafCollimator.jl")
-
-#--- Bixels ------------------------------------------------------------------------------------------------------------
-
-rectangle(x, y, wx, wy) = Shape(x.+0.5*wx*[-1, 1, 1, -1], y.+0.5*wy*[-1, -1, 1, 1])
-
-"""
-    plot_bld!(p, bixel::AbstractBixel[, value]; kwargs...)
-
-Plot a bixel.
-
-To colour the bixel by a value (*e.g.* for a beamlet weight), can pass an optional
-`value` argument.
-"""
-function plot_bld!(p::AbstractPlot, bixel::AbstractBixel; kwargs...)
-    px, py = Roentgen.position(bixel)
-    wx, wy = Roentgen.width(bixel)
-
-    plot!(p, rectangle(px, py, wx, wy); aspect_ratio=1, kwargs...)
-end
-
-function plot_bld!(p::AbstractPlot, bixel::AbstractBixel, value; kwargs...)
-    plot_bld!(p, bixel; line_z=value, fill_z=value, kwargs...)
-end
-
-"""
-    plot_bld!(p, bixel::AbstractArray{<:AbstractBixel}[, value::AbstractArray]; kwargs...)
-
-Plot a collection of bixels.
-
-To colour the bixels by a value (*e.g.* for a beamlet weight), can pass an optional
-`value` array.
-"""
-function plot_bld!(p::AbstractPlot, bixels::AbstractArray{<:AbstractBixel};
-                   kwargs...)
-
-    for i in eachindex(bixels)
-        plot_bld!(p, bixels[i]; primary=false, kwargs...)
-    end
-
-    p
-end
-
-function plot_bld!(p::AbstractPlot, bixels::AbstractArray{<:AbstractBixel},
-                   value::AbstractArray{<:Real}; kwargs...)
-
-    @assert length(bixels) == length(value)
-
-    for i in eachindex(bixels, value)
-        plot_bld!(p, bixels[i], value[i]; primary=false, kwargs...)
-    end
-
-    p
-end
+include("Bixels.jl")
 
 end # module RoentgenPlots
